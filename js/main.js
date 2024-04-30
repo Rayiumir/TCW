32b439b773f348f21ae24fc54c07a43deb0426fb
Raymond Baghumian <swordart2050@gmail.com>
2024-05-01 01:44:47 +0330

    Creating Profolios

--- js/main.js
+++ js/main.js
@@ -4,3 +4,26 @@ AOS.init({
     once: true,
     mirror: false
 });
+
+// Portfolios
+
+const filterableButtons = document.querySelectorAll(".buttons button");
+const filterableCards = document.querySelectorAll(".cards .card");
+
+const filterCrads = e => {
+
+    filterableCards.forEach(card => {
+
+        card.classList.add("hide");
+
+        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all"){
+            card.classList.remove("hide");
+        }
+
+    });
+        
+};
+
+filterableButtons.forEach(button => {
+    button.addEventListener("click", filterCrads);
+});
