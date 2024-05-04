786b73b5b02f5f7426122bd948609d894b8440a1
Raymond Baghumian <swordart2050@gmail.com>
2024-05-04 17:48:58 +0330

    Creating Post Types for Sections Theme

--- functions.php
+++ functions.php
@@ -3,8 +3,16 @@
 // Calling files
 
 require_once('inc/enqueue.php');
+require_once('inc/post_type.php');
 require_once('navbar.php');
+
 
 // Enable Register Nav Menus
 
 register_nav_menu('main-menu', 'منوی اول');
+
+// Enable Thumbnails
+
+if(function_exists('add_theme_support')){
+    add_theme_support('post-thumbnails');
+}
---
+++ inc/post_type.php
@@ -0,0 +1,73 @@
+<?php
+
+// Services
+
+function Services() {
+    register_post_type('service',
+        array(
+            'labels' => array(
+                'name' => 'خدمات ما',
+                'singular_name' => 'خدمات ها'
+            ),
+
+            'public' => true,
+            'rewrite' => array('slug' => 'service'),
+            'supports' => array('title', 'editor')
+        )
+    );
+}
+add_action('init', 'Services');
+
+// Portfolios
+
+function Portfolios() {
+    register_post_type('portfolio',
+        array(
+            'labels' => array(
+                'name' => 'نمونه کار',
+                'singular_name' => 'نمونه کارها'
+            ),
+
+            'public' => true,
+            'rewrite' => array('slug' => 'portfolio'),
+            'supports' => array('title', 'thumbnail', 'editor')
+        )
+    );
+}
+add_action('init', 'Portfolios');
+
+// Teams
+
+function Teams() {
+    register_post_type('team',
+        array(
+            'labels' => array(
+                'name' => 'تیم ما',
+                'singular_name' => 'تیم ها'
+            ),
+
+            'public' => true,
+            'rewrite' => array('slug' => 'team'),
+            'supports' => array('title', 'thumbnail')
+        )
+    );
+}
+add_action('init', 'Teams');
+
+// Customers
+
+function Customers() {
+    register_post_type('customer',
+        array(
+            'labels' => array(
+                'name' => 'مشتریان ما',
+                'singular_name' => 'مشتری ها'
+            ),
+
+            'public' => true,
+            'rewrite' => array('slug' => 'customer'),
+            'supports' => array('title', 'thumbnail')
+        )
+    );
+}
+add_action('init', 'Customers');
